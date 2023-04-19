9bee6ad5673c55772f4b63f7ceab0b181162e1b8
FreeFi <freegasss@gmail.com>
2023-04-19 23:17:09 +1100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Хогвартс',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Перевернутый мир',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Кадирожо Пермаи',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Райский остров',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Лаборатории Хокинса',
+  },
+];
+
+export const startingFeatures = [
+  'Найдите мир, который подходит вам, и в который вы хотите войти',
+  'Войдите в мир, читая басмалу, чтобы быть в безопасности',
+  'Не нужно ходить вокруг да около, просто жми на газ и получай удовольствие',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Новый Мир',
+    subtitle:
+        'у нас есть последнее обновление с новым миром для вас',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Более реалистичный',
+    subtitle:
+        'Последнее обновление делает мир более реалистичным, чем когда-либо',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Запуск Метавселенной заставляет Илона маскировать кетар-кетир',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 советов, как легко освоить безумие Метавселенной',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'С одной платформой вы можете виртуально исследовать весь мир',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
